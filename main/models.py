9a4571af3549974105946288fc9d5b754e0b1c5a
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-08-05 20:47:38 +0200

    Added some basic models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    
+    name = models.CharField(max_length = 25)
+    summary = models.CharField(max_length = 75)
+    description = models.CharField(max_length = 300)
+
+    def __str__(self):
+        return self.name
