45e3c74a9058eac31015dd7eb9b5497d66653e01
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-08-06 16:23:17 +0200

    Scaffolded the basic index page

---
+++ static/js/base.js
@@ -0,0 +1,35 @@
+new WOW().init();
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	// Display background images
+	$('.top-content').backstretch(staticDir + "img/backgrounds/1.jpg");
+	$('.section-4-container').backstretch(staticDir + "img/backgrounds/1.jpg");
+
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+
+
+});
